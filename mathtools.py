c140c09251bd6b7a3f3b2eeb06aba4d2705ad897
toni-kolehmainen <tkolehma20@student.oulu.fi>
2020-09-25 14:30:00 +0300

    test

--- mathtools.py
+++ mathtools.py
@@ -17,3 +17,11 @@ def factorial(n):
         return 1
     else:
         return n * factorial(n-1)
+        
+
+def geometric(a, ratio, n):
+    '''Calculates the sum of a geometric serie of n elements.
+       A geometric sequence is of the form: a, a*r, a*r*r, a*r*r*r,...
+       n is the number of elements in the sequence.'''
+    #Use the sum formula:
+    return a*(1-ratio**n)/(1-ratio)
